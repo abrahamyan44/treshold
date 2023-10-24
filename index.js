45afb63122d7821e459fd4e4f6ba80ad513f1824
Yura <y.abrahamyan@codeex.io>
2023-10-24 23:51:04 +0400

    email functionality is added

--- index.js
+++ index.js
@@ -1,104 +1,69 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const multer = require("multer");
+const nodemailer = require("nodemailer");
+
 const { RenderImage } = require('./render');
-
-var multiparty = require('multiparty');
-var http = require('http');
-var util = require('util');
-
-var multiparty = require('connect-multiparty'),
-    multipartyMiddleware = multiparty({ uploadDir: __dirname + "/temp", });
 
 const PORT = 3001;
 
-const app = express();
-// app.use(bodyParser.json());
+const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+                user: 'noreply.tangie@gmail.com',
+                pass: 'xdwowikfywxfdlgk',
+        }
+});
+console.log("Email transporter is crated");
 
+const sendMail = (to, svgBuffer) => {
+        const mailOptions = {
+                from: 'noreply.tangie@gmail.com',
+                to,
+                subject: 'Tangie file',
+                text: 'Find Svg file in attachments',
+                attachments: [
+                        {
+                                filename: 'file.svg',
+                                content: new Buffer(svgBuffer,'utf-8'),
+                        },
+                ],
+        };
+
+        return new Promise((res, rej) => {
+                transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                                console.log(error);
+                                rej(error);
+                        } else {
+                                console.log('Email sent: ' + info.response);
+                                res();
+                        }
+                });
+        })
+}
+
+const app = express();
 const upload = multer({
         dest: __dirname + "/temp",
 });
-
-// const logger = (req, res, next) => {
-//     req.rawBody = '';
-//     req.setEncoding('utf8');
-//
-//     req.on('data', function(chunk) {
-//         req.rawBody += chunk;
-//     });
-//
-//     req.on('end', function() {
-//         // console.log(" req.rawBody: ",  req.rawBody.toString())
-//         next();
-//     });
-//
-//         console.log(`
-//       ${req.method}
-//       ${req.url}
-//       ${req.ip}`);
-//         next();
-// };
-//
-// // app.use(logger)
 
 app.post(
     '/render',
     upload.single("file"),
     async (req, res) => {
-        console.log(req.rawHeaders)
+        console.log(req.rawHeaders);
         console.log("Request on /render");
         const tempPath = req.file.path;
-        console.log("req.file: ", req.file)
+        console.log("req.file: ", req.file);
         console.log(`Saving tmp file in ${tempPath}`);
         const renderImage = new RenderImage(tempPath, __dirname + "/img.svg");
         const svgBuf = await renderImage.trace();
         console.log("Svg buffer ready");
-        res.end(svgBuf)
+        await sendMail('yurabrahamyan44@gmail.com', svgBuf);
+        console.log("Email sent");
+        res.end("Email with attachment is sent");
 })
-
-// app.post('/render', multipartyMiddleware, function(req, res) {
-//     console.log("req.rawHeaders: ", req.rawHeaders)
-//     console.log(req.body, req.files);
-//     var file = req.files.file;
-//     console.log(file.name);
-//     console.log(file.type);
-//     res.status(200).send('OK');
-// });
 
 app.listen(PORT, "0.0.0.0", () =>{
         console.log(`Server is running on port:  ${PORT}`);
 })
-//
-// var http = require('http')
-// var url = require('url')
-// var queryString = require( "querystring" );
-//
-// var port = 3001
-// if (process.argv[2]) {
-//     port = Number(process.argv[2])
-// }
-//
-// var server = http.createServer(function (req, res) {
-//
-//     console.log("* " + req.method + " ************************************")
-//     console.log(req.url)
-//     console.log(req.headers)
-//
-//     var bodyData = '';
-//
-//     req.on('data', function(chunk) {
-//         bodyData += chunk.toString();
-//     });
-//
-//     req.on('end', function() {
-//         console.log(bodyData)
-//
-//         res.writeHead(200, "OK", {'Content-Type': 'application/json'});
-//         res.end();
-//     });
-//
-// })
-//
-// server.listen(port)
-//
-// console.log("Server listening on " + port)
